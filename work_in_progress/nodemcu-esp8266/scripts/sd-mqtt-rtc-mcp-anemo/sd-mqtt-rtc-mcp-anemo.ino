d0988e5e019a1cab480f75fbe5f1d877ffdab01b
1mio1 <zettrik@users.noreply.github.com>
2018-02-12 23:44:26 +0100

    GPL added; variables cleanup

--- work_in_progress/nodemcu-esp8266/scripts/sd-mqtt-rtc-mcp-anemo/sd-mqtt-rtc-mcp-anemo.ino
+++ work_in_progress/nodemcu-esp8266/scripts/sd-mqtt-rtc-mcp-anemo/sd-mqtt-rtc-mcp-anemo.ino
@@ -1,4 +1,7 @@
 /*
+ * Software for windpower charge controller on NodeMCU
+ *   <https://systemausfall.org/wikis/howto/Eddy-2-Power>
+ *
  * blinking onboard LED means:
  *   fast  - connecting to wireless network
  *   slow  - connecting to mqtt broker
@@ -6,7 +9,41 @@
  *   on    - critical power supply
  * 
  * mqtt channel scheme:
- *   /node/X/sensor - publish from node X the value(s) of corresponding sensor (e.g. adc, mcp, internal vcc)
+ *   /node/X/sensor - publish from node X the value(s) of corresponding
+ *                      sensor (e.g. adc, mcp, internal vcc)
+ *
+ * TODO: wifi sleep mode and deep sleep don't work atm
+ *  ESP.deepSleep(sleep_time);
+ *  ESP.reset();
+*/
+/*
+Copyright 2018 Henning Rieger <age@systemausfall.org>
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+Dieses Programm ist Freie Software: Sie können es unter den Bedingungen
+der GNU General Public License, wie von der Free Software Foundation,
+Version 3 der Lizenz oder (nach Ihrer Wahl) jeder neueren
+veröffentlichten Version, weiterverbreiten und/oder modifizieren.
+
+Dieses Programm wird in der Hoffnung, dass es nützlich sein wird, aber
+OHNE JEDE GEWÄHRLEISTUNG, bereitgestellt; sogar ohne die implizite
+Gewährleistung der MARKTFÄHIGKEIT oder EIGNUNG FÜR EINEN BESTIMMTEN ZWECK.
+Siehe die GNU General Public License für weitere Details.
+
+Sie sollten eine Kopie der GNU General Public License zusammen mit diesem
+Programm erhalten haben. Wenn nicht, siehe <http://www.gnu.org/licenses/>.
 */
 
 #include <ESP8266WiFi.h>
@@ -21,52 +58,45 @@
 /*
  * Change the following values to fit your setup.
  */
-// wifi:
+/* wifi */
 const char* ssid = "";
 const char* password = "";
 
-// mqtt broker:
+/* mqtt broker */
 const int node_id = 5;
 const String mqtt_channel = "node/5/";
 const char* mqtt_server = "192.168.1.111";
 const int mqtt_port = 1883;
 
-// sensor measurement interval
-const int sleep_time = 10;  // sleep time between readings in seconds
+/* sleep time between measurements in milliseconds */
+const int sleep_time = 10000;
 
-// buttons, led ... wiring and mapping to pins:
-const int button1 = 4;     // nodemcu pin: D2
-const int button2 = 0;     // nodemcu pin: D3
-const int led1 = 16;       // nodemcu pin: D0 (and built in LED)
-const int led2 =  5;       // nodemcu pin: D1
-const int chipSelect = 10; // nodemcu pin: 
-const int rtc_SDA = 2;     // nodemcu pin:
-const int rtc_SDC = 9;     // nodemcu pin: 
+/* anemometer, rpmometer */
+const byte interrupt_pin1 = 4;  // nodemcu pin: D2
+const byte interrupt_pin2 = 0;  // nodemcu pin: D3
 
-// mcp3208 wiring:
-#define CS_PIN 15    // nodemcu pin: D8
-#define CLOCK_PIN 14 // nodemcu pin: D5
-#define MOSI_PIN 13  // nodemcu pin: D7
-#define MISO_PIN 12  // nodemcu pin: D6
+/* output pins e.g. leds and relais */
+const int led1 = 16;            // nodemcu pin: D0 (and built-in LED)
+const int led2 =  5;            // nodemcu pin: D1
 
-// anemometer
-const byte interruptPin1 = 4;  // nodemcu pin: D2
-const byte interruptPin2 = 0;  // nodemcu pin: D3
-volatile int interruptCounter1 = 0;
-volatile int interruptCounter2 = 0;
+/* sd card */
+const int chip_select = 10;     // nodemcu pin: 
+
+/* real time clock */
+const int rtc_SDA = 2;          // nodemcu pin:
+const int rtc_SDC = 9;          // nodemcu pin: 
+
+/* mcp3208 SPI connections */
+#define CS_PIN 15               // nodemcu pin: D8
+#define CLOCK_PIN 14            // nodemcu pin: D5
+#define MOSI_PIN 13             // nodemcu pin: D7
+#define MISO_PIN 12             // nodemcu pin: D6
 
 /*
  * Don't change after here, unless you know what you do.
  */
-int led1_state = 0;
-int led2_state = 0;
-int button1_state = 0;
-int button2_state = 0;
-int button1_state_last = 0;
-int button2_state_last = 0;
 int analog_in = 0;
 int analog_in_sum = 0;
-int val = 0;
 unsigned long now = 0;
 unsigned long before = 0;
 unsigned long rtc_now;
@@ -75,6 +105,8 @@ char mqtt_message[120];
 char sd_data;
 int adc0, adc1, adc2, adc3;
 int mcp[8];
+volatile int interruptCounter1 = 0;
+volatile int interruptCounter2 = 0;
 
 ADC_MODE(ADC_VCC); //read ESP voltage instead of analog_in A0, leave A0 unconnected
 MCP3208 mcp3208(CLOCK_PIN, MOSI_PIN, MISO_PIN, CS_PIN);
@@ -94,14 +126,12 @@ Adafruit_ADS1015 ads;
 void setup() {
   pinMode(led1, OUTPUT);
   pinMode(led2, OUTPUT);
-  pinMode(button1, INPUT);
-  pinMode(button2, INPUT);
   digitalWrite(led1, 1);
   digitalWrite(led2, 1);
   Serial.begin(115200);
   setup_wifi();
   setup_mqtt();
-  setup_interrupts(); // for anemometer
+  setup_interrupts();     // for anemometer
   //setup_adc();
   //setup_sd();
   //setup_rtc();
@@ -111,32 +141,23 @@ void setup() {
  * main loop
  */
 void loop() {
-  before = now;
-  now = millis();
-  // keep in mind: millis() will start again from 0 about every 50 days
-  if (now < before) {
-    before = now;
-  }
-  time_delta = now - before;
-  
-  Serial.print("uptime: ");
-  Serial.println(now);
-  Serial.print("time for last loop: ");
-  Serial.println(time_delta);
-  snprintf(mqtt_message, 100, "%ld,%ld", now, time_delta);
-  mqtt_pub(mqtt_channel + "uptime", mqtt_message);
-  
+  check_time();
   read_mcp();
   check_battery();
   check_voltage();
+  
+  //read_adc();
    
-  delay(sleep_time * 1000);
+  //Serial.print("rtc: ");
+  //Serial.println(get_rtc());
+
+  delay(sleep_time);
   
   check_interrupts();
 }
 
 /*
- * Spannungsschalter
+ * switch relais depending on measured voltage
  */
 void check_voltage() {
   Serial.println(mcp[1]);
@@ -154,10 +175,10 @@ void check_voltage() {
  * Anemometer
  */
 void setup_interrupts(){
-  pinMode(interruptPin1, INPUT_PULLUP);
-  attachInterrupt(digitalPinToInterrupt(interruptPin1), handle_interrupt1, FALLING);
-  pinMode(interruptPin2, INPUT_PULLUP);
-  attachInterrupt(digitalPinToInterrupt(interruptPin2), handle_interrupt2, FALLING);
+  pinMode(interrupt_pin1, INPUT_PULLUP);
+  attachInterrupt(digitalPinToInterrupt(interrupt_pin1), handle_interrupt1, FALLING);
+  pinMode(interrupt_pin2, INPUT_PULLUP);
+  attachInterrupt(digitalPinToInterrupt(interrupt_pin2), handle_interrupt2, FALLING);
 }
 
 void handle_interrupt1() {
@@ -184,6 +205,25 @@ void check_interrupts(){
   interruptCounter2 = 0;
 }
 
+/*
+ * handle loop time
+ * keep in mind: millis() will start again at 0 about every 50 days
+ */
+void check_time() {
+  before = now;
+  now = millis();
+  if (now < before) {
+    before = now;
+  }
+  time_delta = now - before;
+  
+  Serial.print("uptime: ");
+  Serial.println(now);
+  Serial.print("time for last loop: ");
+  Serial.println(time_delta);
+  snprintf(mqtt_message, 100, "%ld,%ld", now, time_delta);
+  mqtt_pub(mqtt_channel + "uptime", mqtt_message);
+}
 
 /*
  * ADC
@@ -296,7 +336,7 @@ unsigned long get_rtc() {
  * SD Card
  */
 void setup_sd(){
-  if (!SD.begin(chipSelect)) {
+  if (!SD.begin(chip_select)) {
     Serial.println("SD card initialization failed!");
     return;
   }
@@ -437,7 +477,7 @@ void mqtt_reconnect() {
     }
     else {
       // retry mqtt connection three times, otherwise move on
-      Serial.print("Conenction to MQTT borker failed. Client state: ");
+      Serial.print("Conenction to MQTT broker failed. Client state: ");
       Serial.println(client.state());
       Serial.println("I will try again in 3 seconds.");
       // blink'n wait 3 seconds before retrying
@@ -495,28 +535,6 @@ void check_battery() {
   //Serial.println(analog_in);
   snprintf(mqtt_message, 100, "%04i", analog_in);
   mqtt_pub(mqtt_channel + "vcc", mqtt_message);
-
-  return;
-  
-  // warn about high power and wait a second
-  if (analog_in >= 51) {
-    Serial.println("Input power is too high!");
-    snprintf(mqtt_message, 75, "node %02d - Input power is too high! %04d, %ld", node_id, analog_in, now);
-    mqtt_pub("nodes", mqtt_message);
-    digitalWrite(led1, 0);
-    delay(1000);
-    digitalWrite(led1, 1);
-  }
-  // warn about low power
-  // very low voltage probably means we are running by usb
-  else if (analog_in <= 41) {
-    Serial.println("Input power is too low!");
-    snprintf(mqtt_message, 75, "node %02d - Input power is too low! %04d, %ld", node_id, analog_in, now);
-    mqtt_pub("nodes", mqtt_message);
-    digitalWrite(led1, 0);
-    delay(1000);
-    digitalWrite(led1, 1);
-  }
 }
 
 float multiple_read(int times) {
@@ -530,83 +548,3 @@ float multiple_read(int times) {
   return analog_in_sum;
 }
 
-
-/*
- * probably obsolete code...
- */
- 
-/*
-  Serial.print("rtc: ");
-  Serial.println(get_rtc());
-*/
-
-/*
-  //read_adc();
-  //button1_state = digitalRead(button1);
-  //button2_state = digitalRead(button2);
-*/
-
-/*
-  if ((unsigned long)(now - before) >= 60000) {
-    check_battery();
-    before = now;
-    if (SD.exists("datalog.txt")) {
-      Serial.println("File datalog.txt exists on card");
-      snprintf(mqtt_message, 75, "%01d,%01d,%04d,%ld,%ld", node_id, button1_state, button2_state, analog_in, now, get_rtc());
-      mqtt_pub("/node/5/adc", mqtt_message);
-    }
-    //write_sd();
-  }
-  if (button1_state != button1_state_last) {
-    button1_state_last = button1_state;
-    snprintf(mqtt_message, 40, "%01d", button1_state);
-    mqtt_pub("/node/5/button/1", mqtt_message);
-  }
-  else if (button2_state != button2_state_last) {
-    button2_state_last = button2_state;
-    snprintf(mqtt_message, 40, "%01d", button2_state);
-    mqtt_pub("/node/5/button/2", mqtt_message);
-  }
-*/
- 
-
-/*
-  // let em data fly
-  wifi_awake();
-  Serial.println(client.state());
-  if (client.connected ()){
-     client.loop();
-  }
-  else {
-    Serial.println("MQTT Broker Connection Down");
-    if (client.connect("node6","shed/will",0,0,"Shed Temp Fail")) {
-       client.publish("nodes","node6 reconnected");
-       Serial.println("MQTT Broker Connection Restated");
-    }
-    else {
-      // Don't flood the broker with connections and it's already down so we don't need to worry about a delay disconnecting the client
-      delay(1000);
-      Serial.println("MQTT Connection Delay");
-    }
-  }
-  Serial.println(client.state());
-  //setup_mqtt();
-  //Serial.println(client.state());
-  //mqtt_reconnect();
-  //Serial.println(client.state());
-  //setup_mqtt();
-  //client.loop();
-  Serial.println(client.state());
-  snprintf(mqtt_message, 100, "node %02d - %01d,%01d,%04d,%ld", node_id, button1_state, button2_state, analog_in, now);
-  mqtt_pub("nodes", mqtt_message);
-  
-  client.disconnect();
-  Serial.println(client.state());
-  wifi_sleep();
-*/
-
-/*
-  //todo:  real deep sleep also does not work atm
-  //ESP.deepSleep(sleepTime * 1000);
-  //ESP.reset();
-*/
